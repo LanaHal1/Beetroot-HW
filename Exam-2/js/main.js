f74e8ed156c2ce38ce5cae718e7fbfd1bcd64bb0
Lana Halytskaya <muha6889@gmail.com>
2018-03-28 00:05:46 +0300

    updated Exam-2

--- Exam-2/js/main.js
+++ Exam-2/js/main.js
@@ -21,6 +21,16 @@ initMap = () => {
   });
 };
 
+$(".header__nav a").click(function(event) {
+  event.preventDefault();
+  $("html, body").animate(
+    {
+      scrollTop: $($.attr(this, "href")).offset().top
+    },
+    700
+  );
+});
+
 // PORTFOLIO__Gallery
 $(".grid").masonry({
   // options
@@ -96,3 +106,29 @@ $(document).ready(function() {
     slidesToScroll: 1
   });
 });
+
+// BUTTON BACK to TOP
+if ($("#button__page-up").length) {
+  let scrollTrigger = 300; // px
+  let backToTop = function() {
+    let scrollTop = $(window).scrollTop();
+    if (scrollTop > scrollTrigger) {
+      $("#button__page-up").addClass("is-visible");
+    } else {
+      $("#button__page-up").removeClass("is-visible");
+    }
+  };
+  backToTop();
+  $(window).scroll(function() {
+    backToTop();
+  });
+  $("#button__page-up").click(function(e) {
+    e.preventDefault();
+    $("html,body").animate(
+      {
+        scrollTop: 0
+      },
+      700
+    );
+  });
+}
